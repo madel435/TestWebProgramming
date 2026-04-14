d0aac136cc24856a575efb18e9162457859eb00d
Madison <adelstonem2@winthrop.edu>
2026-04-14 02:38:10 -0400

    RM old files

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.get("/", (req, res) => {
 
 // Knowledge quiz intro page
 app.get("/Quiz1/quiz1", (req, res) => {
-  res.render("Quiz1/quiz1");
+  res.render("/Quiz1/quiz1");
 });
 // LOGIN PAGE
 app.get("/Quiz2/login", (req, res) => {
-  res.render("Quiz2/login");
+  res.render("/Quiz2/login");
 });
 
 // LOGIN HANDLER
@@ -50,12 +50,12 @@ app.post("/Quiz2/login", (req, res) => {
 
 // First quiz question (example)
 app.get("/Quiz1/quiz1Q", (req, res) => {
-  res.render("Quiz1/quiz1Q");
+  res.render("/Quiz1/quiz1Q");
 });
 
 // Protected route: quiz2 results/page
 app.get("/Quiz2/quiz2Q", requireLogin, (req, res) => {
-  res.render("Quiz2/quiz2Q");
+  res.render("/Quiz2/quiz2Q");
 });
 
 // Resource page
